f3e0116f6bfdc46ead2dc1680a86fcd25c98331b
unknown <20211A6604@bvrit.ac.in>
2022-04-14 16:42:20 +0530

    user screens

--- lib/main.dart
+++ lib/main.dart
@@ -23,9 +23,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-
-      home: const TransactionComplete(),
-
+      home: const PaymentScreen(),
     );
   }
 }
--- lib/screens/users/payment_screen.dart
+++ lib/screens/users/payment_screen.dart
@@ -2,6 +2,174 @@ import 'package:flutter/material.dart';
 import 'package:numeric_keyboard/numeric_keyboard.dart';
 import 'package:spring/ui_utils.dart';
 
+class PaymentScreen extends StatefulWidget {
+  const PaymentScreen({Key? key}) : super(key: key);
+
+  @override
+  State<PaymentScreen> createState() => _PaymentScreenState();
+}
+
+class _PaymentScreenState extends State<PaymentScreen> {
+  @override
+  Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      body: ListView(
+        children: [
+          Center(
+            child: SizedBox(
+                width: width * 0.9,
+                height: height,
+                child: Column(
+                  children: [
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Container(
+                        width: 30,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border:
+                                Border.all(width: 2, color: UiUtils.medium)),
+                        child: FloatingActionButton(
+                            elevation: 0,
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            backgroundColor: Colors.white,
+                            mini: true,
+                            child: Icon(
+                              Icons.check_rounded,
+                              size: 18,
+                              color: UiUtils.medium,
+                            )),
+                      ),
+                    ),
+                    SizedBox(
+                      height: height * 0.05,
+                    ),
+                    Text(
+                      "Pay to others",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: UiUtils.dark,
+                        fontSize: 24,
+                        fontFamily: UiUtils.fontFamily,
+                        fontWeight: FontWeight.w800,
+                        letterSpacing: 0.12,
+                      ),
+                    ),
+                    SizedBox(
+                      height: height * 0.2,
+                    ),
+                    Container(
+                      width: width * 0.9,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: Color(0xfff2f2f2),
+                      ),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          // prefixIcon: SvgPicture.asset(
+                          //   "assets/images/username.svg",
+                          //   height: 3,
+                          //   width: 5,
+                          // ),
+                          prefixIcon: Icon(Icons.person_outline_rounded),
+                          hintText: "Name",
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: height * 0.02,
+                    ),
+                    Container(
+                      width: width * 0.9,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: Color(0xfff2f2f2),
+                      ),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          // prefixIcon: SvgPicture.asset(
+                          //   "assets/images/username.svg",
+                          //   height: 3,
+                          //   width: 5,
+                          // ),
+                          prefixIcon: Icon(Icons.email_outlined),
+                          hintText: "User ID",
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: height * 0.02,
+                    ),
+                    Container(
+                      width: width * 0.9,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: Color(0xfff2f2f2),
+                      ),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          // prefixIcon: SvgPicture.asset(
+                          //   "assets/images/password.svg",
+                          // ),
+                          prefixIcon: Icon(Icons.currency_rupee_rounded),
+                          hintText: "Amount",
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: height * 0.1,
+                    ),
+                    Container(
+                      width: width * 0.5,
+                      height: height * 0.1,
+                      decoration: BoxDecoration(
+                        color: UiUtils.medium,
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      child: ClipRRect(
+                          borderRadius: BorderRadius.circular(15),
+                          child: MaterialButton(
+                            onPressed: () {},
+                            child: Center(
+                              child: Text(
+                                "Pay",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                  fontFamily: UiUtils.fontFamily,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                            ),
+                          )),
+                    ),
+                  ],
+                )),
+          )
+        ],
+      ),
+    );
+  }
+}
+
 class PaymentStart extends StatefulWidget {
   const PaymentStart({Key? key}) : super(key: key);
 
